b89feb97979aeceaed861bf858152221bdc2a952
HerbertNtim <herbertntim15@gmail.com>
2025-10-09 17:54:44 +0000

    updated import from relative to absolute

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
+import logger from '#config/logger.js';
 import express from 'express';
 
 const app = express();
 
 app.get('/', (req, res) => {
+  logger.info('Hello from Acquisitions!'),
+
   res.status(200).send('Hello from Acquisitions!'); 
 });
 
