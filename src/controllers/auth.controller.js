902060969ffc77e4f4d0573cb4becc51159b6243
HerbertNtim <herbertntim15@gmail.com>
2025-10-10 16:16:46 +0000

    authentication done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,9 @@
 import logger from '#config/logger.js';
 import { signUpSchema } from '#validations/auth.validations.js';
 import { formatValidationError } from '#utils/format.js';
+import { createUser } from '#services/auth.service.js';
+import { jwttoken } from '#utils/jwt.js';
+import { cookies } from '#utils/cookies.js';
 
 export const signup = async (req, res, next) => {
   try {
@@ -13,14 +16,20 @@ export const signup = async (req, res, next) => {
       });
     }
 
-    const { name, email, role } = validationResult.data;
+    const { name, email, password, role } = validationResult.data;
 
     // AUTH SERVICE
+    const user = await createUser({name, email, password, role});
+    const token = jwttoken.sign({ id: user.id, email: user.email, role: user.role });
+
+    cookies.set(res, 'token', token);
+
+
     logger.info(`User registered successfully: ${email}`);
     res.status(201).json({
       message: 'User registered',
       user: {
-        id: 1, name, email, role
+        id: user.id, email: user.email, role: user.role
       }
     });
   } catch (error) {
