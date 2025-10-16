7780899ca6bcfd3a67e34cc882dd96bd49fb7ff2
HerbertNtim <herbertntim15@gmail.com>
2025-10-16 12:37:43 +0000

    get user by id controller implemented

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,19 +1,50 @@
 import { db } from '#config/database.js';
 import logger from '#config/logger.js';
 import { users } from '#models/users.model.js';
+import { eq } from 'drizzle-orm';
 
-export const getAllUsers = async() => {
+export const getAllUsers = async () => {
   try {
-    return await db.select({
-      id: users.id,
-      email: users.email,
-      name: users.name,
-      role: users.role,
-      created_at: users.created_at,
-      updated_at: users.updated_at,
-    });
+    return await db
+      .select({
+        id: users.id,
+        email: users.email,
+        name: users.name,
+        role: users.role,
+        created_at: users.created_at,
+        updated_at: users.updated_at,
+      })
+      .from(users);
   } catch (error) {
     logger.error('Error getting users', error);
     throw error;
   }
 };
+
+export const getUserById = async id => {
+  try {
+    const [user] = await db
+      .select({
+        id: users.id,
+        email: users.email,
+        name: users.name,
+        role: users.role,
+        created_at: users.created_at,
+        updated_at: users.updated_at,
+      })
+      .from(users)
+      .where(eq(users.id, id))
+      .limit(1);
+
+    if(!user) {
+      throw new Error('User not found');
+    }
+
+    return user;
+  } catch (error) {
+    logger.error(`Error getting user by id ${id}:`, error);
+    throw error;
+  }
+};
+
+
